27a9bb9c14d0e27a7a96cb48136f5b866d1abe86
Shibly <shibly.phy@gmail.com>
2015-08-30 18:31:39 +0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+/**
+ * Created by limbo on 8/29/15.
+ */
+
+
+'use strict';
+
+angular.module('movieTime', [])
+    .controller('MovieController', function($scope, $http){
+        var pendingTask;
+
+        if($scope.search === undefined){
+            $scope.search = "Sherlock Holmes";
+            fetch();
+        }
+
+        $scope.change = function(){
+            if(pendingTask){
+                clearTimeout(pendingTask);
+            }
+            pendingTask = setTimeout(fetch, 800);
+        };
+
+        function fetch(){
+            $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+                .success(function(response){
+                    $scope.details = response;
+                    $scope.actors = response.Actors;
+                });
+
+            $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+                .success(function(response){  $scope.related = response; });
+        }
+
+        $scope.update = function(movie){
+            $scope.search = movie.Title;
+            $scope.change();
+        };
+
+        $scope.select = function(){
+            this.setSelectionRange(0, this.value.length);
+        }
+    });
---
+++ readme.md
@@ -0,0 +1,5 @@
+## MovieTime
+
+MovieTime is a simple AngularJS app that leverage it's http service. Within this app you can search for any movie and get the result instantly. You
+will also get the related movies, lead actors, buy and trailer links with full movie summery!
+
